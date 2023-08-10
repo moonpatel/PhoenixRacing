f8ec2c4132f71ffb009b2b884ba5d9e4fa5fc4a2
Moon Patel <moonpatel2003@gmail.com>
2023-08-10 23:09:10 +0530

    data added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -17,24 +17,14 @@ export default function Home() {
   const description1 = 'Team Phoenix Racing is a technical student chapter affiliated with SVNIT, consisting of over 60 students from various engineering disciplines. Our primary objective is to provide a platform for aspiring engineers to excel, acquire practical knowledge, and gain exposure to real-world challenges. Since our establishment in 2005, we have actively participated in and successfully competed in a range of competitions, including Baja, Supra SAE, Go-Kart, and Formula Bharat. These endeavors have involved the complete design, manufacturing, and management of projects, all carried out by our dedicated student team. Embracing the principles of sustainability and electrification, we have made a significant transition towards the production of electric vehicles starting in 2022.';
   const description2 = '';
   const aboutSvnit = 'Established in 1961 as one of the RECs,Sardar Vallabhbhai National Institute of Technology aimed to provide technical education in Civil, Mechanical, and Electrical Engineering. Over time, additional undergraduate programs were introduced, including Electronics Engineering in 1983-84, Computer Engineering and Production Engineering in 1988-89, and Chemical Engineering in 1995-96. In 2002, the institute received recognition as a "Deemed University" with the new name Sardar Vallabhbhai National Institute of Technology (SVNIT), Surat, through the University Grants Commission (UGC) Act. Subsequently, in 2007, it obtained the prestigious status of an "Institute of National Importance." Currently, SVNIT offers six undergraduate programs, nineteen postgraduate programs, three M.Sc. programs, and a five-year integrated program that includes a doctoral program in all the aforementioned disciplines.';
-  let slides = [
-    <img
-      src="https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_Ghostrunner_image1600w.jpg"
-      alt="1"
-    />,
-    <img
-      src="https://i.pinimg.com/originals/cd/74/50/cd7450cf06d003bba1af588f0ac6722a.jpg"
-      alt="2"
-    />,
-    <img
-      src="https://cdn2.unrealengine.com/egs-deadcells-motiontwin-s1-2560x1440-312502186.jpg"
-      alt="3"
-    />,
-    <img
-      src="https://cdn2.unrealengine.com/egs-deadcells-motiontwin-s1-2560x1440-312502186.jpg"
-      alt="4"
-    />
+  let urls = [
+    "home-slider/BIC1.jpg",
+    "home-slider/sldr_1.png",
+    "home-slider/sldr_6.jpg",
+    "home-slider/sldr_7.JPG",
+    "home-slider/sldr_8.JPG"
   ];
+  let slides = urls.map((url, index) => (<img src={url} alt={index} />))
 
   return (
     <div className="home">
@@ -52,7 +42,7 @@ export default function Home() {
       <div className="home-carousel-container">
         <Carousel slides={slides} autoplay={true} interval={4000} />
       </div>
-      <AbtSv heading={heading2} descr1={aboutSvnit} descr2="" embedLink='./image3.jpg' />
+      <AbtSv heading={heading2} descr1={aboutSvnit} descr2="" embedLink='sponsors/2023-24/NIT_Surat_Logo.svg.png' />
       <Testimonials />
       <Faculty />
 
--- frontend/src/components/connect.jsx
+++ frontend/src/components/connect.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./alumni.css";
 
-function Connect({title,btnName}) {
+function Connect({ title, btnName }) {
   return (
     <div className="alumni-connect">
       <h2>{title}</h2>
-      <form action="#">
+      <form action="mailto:sae@svnit.ac.in">
         <button type="submit">{btnName}</button>
       </form>
     </div>
